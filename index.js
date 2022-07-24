205d592226621810b7c99961d429a4461626ccde
Andrei <105882288+AndreiMihaiR@users.noreply.github.com>
2022-07-24 13:22:14 +0300

    second commit

--- index.js
+++ index.js
@@ -1,80 +1,80 @@
 const movieObject = {
-    adult: false,
-    backdrop_path: "/jBFxXKCrViA88hhO59fDx0Av4P.jpg",
-    belongs_to_collection: {
-      id: 10,
-      name: "Star Wars Collection",
-      poster_path: "/bYbHqvRANCpuRTs0RAu10LhmVKU.jpg",
-      backdrop_path: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg"
+  adult: false,
+  backdrop_path: "/jBFxXKCrViA88hhO59fDx0Av4P.jpg",
+  belongs_to_collection: {
+    id: 10,
+    name: "Star Wars Collection",
+    poster_path: "/bYbHqvRANCpuRTs0RAu10LhmVKU.jpg",
+    backdrop_path: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg"
+  },
+  budget: 11000000,
+  genres: [
+    {
+      id: 12,
+      name: "Adventure"
     },
-    budget: 11000000,
-    genres: [
-      {
-        id: 12,
-        name: "Adventure"
-      },
-      {
-        id: 28,
-        name: "Action"
-      },
-      {
-        id: 878,
-        name: "Science Fiction"
-      }
-    ],
-    homepage: "http://www.starwars.com/films/star-wars-episode-iv-a-new-hope",
-    id: 11,
-    imdb_id: "tt0076759",
-    original_language: "en",
-    original_title: "Star Wars",
-    overview: "Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.",
-    popularity: 109.854,
-    poster_path: "/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg",
-    production_companies: [
-      {
-        id: 1,
-        logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
-        name: "Lucasfilm Ltd.",
-        origin_country: "US"
-      },
-      {
-        id: 25,
-        logo_path: "/qZCc1lty5FzX30aOCVRBLzaVmcp.png",
-        name: "20th Century Fox",
-        origin_country: "US"
-      }
-    ],
-    production_countries: [
-      {
-        iso_3166_1: "US",
-        name: "United States of America"
-      }
-    ],
-    release_date: "1977-05-25",
-    revenue: 775398007,
-    runtime: 121,
-    spoken_languages: [
-      {
-        english_name: "English",
-        iso_639_1: "en",
-        name: "English"
-      }
-    ],
-    status: "Released",
-    tagline: "A long time ago in a galaxy far, far away...",
-    title: "Star Wars",
-    video: false,
-    vote_average: 8.2,
-    vote_count: 17382
-  }
-  let submit = document.querySelector('button');
-  let input = document.querySelector('input');
+    {
+      id: 28,
+      name: "Action"
+    },
+    {
+      id: 878,
+      name: "Science Fiction"
+    }
+  ],
+  homepage: "http://www.starwars.com/films/star-wars-episode-iv-a-new-hope",
+  id: 11,
+  imdb_id: "tt0076759",
+  original_language: "en",
+  original_title: "Star Wars",
+  overview: "Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.",
+  popularity: 109.854,
+  poster_path: "/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg",
+  production_companies: [
+    {
+      id: 1,
+      logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
+      name: "Lucasfilm Ltd.",
+      origin_country: "US"
+    },
+    {
+      id: 25,
+      logo_path: "/qZCc1lty5FzX30aOCVRBLzaVmcp.png",
+      name: "20th Century Fox",
+      origin_country: "US"
+    }
+  ],
+  production_countries: [
+    {
+      iso_3166_1: "US",
+      name: "United States of America"
+    }
+  ],
+  release_date: "1977-05-25",
+  revenue: 775398007,
+  runtime: 121,
+  spoken_languages: [
+    {
+      english_name: "English",
+      iso_639_1: "en",
+      name: "English"
+    }
+  ],
+  status: "Released",
+  tagline: "A long time ago in a galaxy far, far away...",
+  title: "Star Wars",
+  video: false,
+  vote_average: 8.2,
+  vote_count: 17382
+}
+let submit = document.querySelector('button');
+let input = document.querySelector('input');
 
 
 submit.addEventListener('click', () => {
-    console.log(input.value)
-    movieObject.movieDirector = input.value;
-    console.log(movieObject);
+  console.log(input.value)
+  movieObject.movieDirector = input.value;
+  console.log(movieObject);
 })
 
 // console.log(movieObject)
@@ -98,7 +98,7 @@ movieObject.mustSee = true;
 //      ****TIP use this keyword remember the right way you can use `this` in a method
 
 movieObject.getGenres = function() {
-  console.log(this.genres);
+console.log(this.genres);
 }
 
 // movieObject.getGenres();
@@ -125,6 +125,8 @@ let newVariableOfProductionCompanies = movieObject.production_companies;
 //      - Third step: when you click on the submit button add them to the right div.
 
 
+let contentContainer = document.getElementById('blogContainer');
+// console.log(contentContainer);
 
 let firstName = document.getElementById('firstName');
 // console.log(firstName);
@@ -139,46 +141,29 @@ let submitBtn = document.getElementById("submitBtn");
 
 
 submitBtn.addEventListener('click', (event) =>{
-  event.preventDefault();
-  let firstN = firstName.value;
-  let lastN = lastName.value;
-  let messageInput = message.value;
-  // let genderSelect = gender.input;
+event.preventDefault();
+let firstN = firstName.value;
+let lastN = lastName.value;
+let messageInput = message.value;
+    document.getElementById('firstName').value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById('messageBox').value = "";
+      
+      let newParagraph = document.createElement('p');
+      newParagraph.className = "mesage-container"
+      contentContainer.appendChild(newParagraph);
+      
+      
+      let finalMessage = document.querySelector('.result');
+      finalMessage.appendChild(newParagraph);
+      
+      let genderSelect = document.querySelector('input[value]:checked');
+      let genderSelectValue = genderSelect.value
+      // console.log(genderSelect1);
+      
+      newParagraph.innerText = `${messageInput} by ${firstN} ${lastN}, ${genderSelectValue}`;
+      // console.log(firstN, lastN, messageInput, genderSelectValue);
+    })
+    
+    
 
-  // genderSelect =(gender) => {
-  //   if (gender == "M") {
-  //     return "M";
-  //   } else if (gender == "F") {
-  //     return "F"
-  //   }
-  // }
-  // console.log(genderSelect());
-
-
-  let newParagraph = document.createElement('p');
-  let finalMessage = document.querySelector('.result');
-
-  
-  finalMessage.appendChild(newParagraph);
-
-  newParagraph.innerText = `His name is ${firstN} ${lastN} and his message is ${messageInput}.`;
-  console.log(firstN, lastN, messageInput);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let finalMessage = document.getElementsByClassName("test");
-
-  // finalMessage.appendChild(newParagraph);
-  // let finalMessage = document.getElementById('submitResult');
